06c15d9efca1198160d71bcf085451cac2416351
Andy0517 <ad870517@gmail.com>
2017-11-14 10:46:39 +0800

    Guess

--- AndroidStudioProjects/Guess100/app/src/main/java/com/lin/guess100/Guess100.java
+++ AndroidStudioProjects/Guess100/app/src/main/java/com/lin/guess100/Guess100.java
@@ -1,5 +1,6 @@
 package com.lin.guess100;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,17 +10,40 @@ import android.widget.TextView;
 
 public class Guess100 extends AppCompatActivity {
     int secnumber = 55 ;//new Random().nextInt(100)+1;
+    int number;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guess100);
         TextView secret = (TextView) findViewById(R.id.secret);
         TextView info = (TextView) findViewById(R.id.info);
-        EditText number = (EditText) findViewById(R.id.number);
+        EditText ednumber = (EditText) findViewById(R.id.number);
+        number = Integer.parseInt(ednumber.toString());
         Button send = (Button) findViewById(R.id.send);
         secret.setText("55");
-    }
-    public void send(View view){
 
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int change = 0;
+                int upline = 100;
+                int doline = 1;
+        if(number>secnumber){
+            new AlertDialog.Builder(Guess100.this)
+                    .setMessage(doline+"to"+number)
+                    .setPositiveButton("繼續",null)
+                    .show();
+            number =upline;
+            upline =change;
+
+        }else if(number<secnumber)
+            new AlertDialog.Builder(Guess100.this)
+                    .setMessage(number+"to"+upline)
+                    .setPositiveButton("繼續",null)
+                    .show();
+                number =doline;
+                doline =change;
+            }
+
+        });}
     }
-}
