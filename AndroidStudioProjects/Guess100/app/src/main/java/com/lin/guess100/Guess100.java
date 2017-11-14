9c5d1563db385b57bd91da357b4dbdce70082107
Andy0517 <ad870517@gmail.com>
2017-11-14 10:12:29 +0800

    findViews

--- AndroidStudioProjects/Guess100/app/src/main/java/com/lin/guess100/Guess100.java
+++ AndroidStudioProjects/Guess100/app/src/main/java/com/lin/guess100/Guess100.java
@@ -2,6 +2,10 @@ package com.lin.guess100;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Guess100 extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class Guess100 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guess100);
+        TextView secret = (TextView) findViewById(R.id.secret);
+        TextView info = (TextView) findViewById(R.id.info);
+        EditText number = (EditText) findViewById(R.id.number);
+        Button send = (Button) findViewById(R.id.send);
+    }
+    public void send(View view){
+
     }
 }
