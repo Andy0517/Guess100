8a20d0701fc2c10a9bbef9cdc2c2957d92af401d
Andy0517 <ad870517@gmail.com>
2017-11-14 10:05:25 +0800

    Layout

---
+++ AndroidStudioProjects/Guess100/app/src/main/java/com/lin/guess100/Guess100.java
@@ -0,0 +1,13 @@
+package com.lin.guess100;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Guess100 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guess100);
+    }
+}
